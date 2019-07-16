7900390583a1b70db7ccc02e70b07f98e83c324f
Ubuntu <ubuntu@ip-172-31-29-43.eu-west-1.compute.internal>
2019-07-16 10:23:03 +0000

    update

--- sap_cai_boilerplate/app.py
+++ sap_cai_boilerplate/app.py
@@ -10,18 +10,19 @@ def index():
     return render_template('index.html') # use methods = GET
 
 
-@app.route('/search', methods=['POST'])
-def search():
+@app.route('/talk', methods=['POST'])
+def talk():
 
-    answer = 'Roger that'
+    # sample code for parsing data received from chatbot
+    #data = json.loads(request.get_data().decode())
+    #print(data)
 
-    data = json.loads(request.get_data().decode())
-    print(data)
+    # sample code for making an API call and parsing response
+    #r = requests.get("https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,EUR")
+    #r = r.json()
+    #print(r.keys())
 
-    r = requests.get("https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,EUR")
-    r = r.json()
-    print(r.keys())
-
+    answer = 'Roger That!'
     return respond(answer)
 
 def respond(answer):
@@ -45,8 +46,10 @@ def errors():
 if __name__ == "__main__":
 
     # local testing
-    app.run(debug=True, host = '0.0.0.0', port = 5000)
+    #app.run(debug=True, host = '0.0.0.0', port = 5000)
 
     # for Heroku deployment
     #port = int(os.environ['PORT'])
     #app.run(port=port, host="0.0.0.0")
+
+    pass
